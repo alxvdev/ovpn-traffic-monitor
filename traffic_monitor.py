3c497a71884f24c2a30f594de30fe5d596736aef
Alexeev Bronislav <bro.alexeev@inbox.ru>
2024-07-11 22:30:50 +0700

    test/fix: edit tcpdump command

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -206,7 +206,7 @@ class TCPDumpManager:
 		:param real_ip: user real IP Address
 		:param virtual_ip: user virtual IP Address
 		"""
-		tcpdump_filter = f'src {virtual_ip} and host '
+		tcpdump_filter = f'src {virtual_ip} or '
 		tcpdump_filter += ' or host '.join(self.config.MONITORING_SITES)
 		process = subprocess.Popen(['tcpdump', '-i', self.config.NETWORK_INTERFACE, tcpdump_filter],
 									stdout=subprocess.PIPE, stderr=subprocess.PIPE)
