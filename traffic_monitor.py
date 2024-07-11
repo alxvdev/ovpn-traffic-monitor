cd63636b626a98563c81de684ae30fec07b3487f
Alexeev Bronislav <bro.alexeev@inbox.ru>
2024-07-11 23:36:28 +0700

    fix: fix waiting users

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -409,6 +409,9 @@ async def main():
 			await asyncio.create_task(openvpn_user_manager.update_user_monitoring())
 			await openvpn_user_manager.update_user_data()
 
+			print(openvpn_user_manager.users_data)
+			print()
+
 			# if len(tcpdump_manager.active_processes) > 0:
 			# 	print(f'Active processes: {tcpdump_manager.active_processes}')
 
